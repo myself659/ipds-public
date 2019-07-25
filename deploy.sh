7a3177c73b23589e33d11342b75d77903e6c8e6c
myself659 <myself659@gmail.com>
2019-07-25 19:40:25 +0800

    fix  path

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SSH="ssh $BCUSER@$BCHOST"
-cd /root/gocode/src/github.com/myself659/ipds-blog-website/public
+cd /root/gocode/src/github.com/myself659/ipds-blog-website/ipds-public
 git pull
